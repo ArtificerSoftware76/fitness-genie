3e1253e184de477b505a3b9a192c58eb3e2ddd1f
ArtificerSoftware76 <alexcromartie76@gmail.com>
2026-09-07 16:35:00 -0400

    Publish artifacts/fitness-genie/assets/branding.ts

---
+++ artifacts/fitness-genie/assets/branding.ts
@@ -0,0 +1,3 @@
+export const fitnessGenieLogo = require('./images/fitness-genie-logo.png');
+export const fitnessGenieIcon = require('./images/fitness-genie-icon-clean.png');
+export const fitnessGenieSearchImage = require('./images/fitness-genie-search.png');
